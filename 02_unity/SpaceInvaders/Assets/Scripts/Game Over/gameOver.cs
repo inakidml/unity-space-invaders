e24d7ae4e0a072cfb112ad890557f271f1ff310c
Iñaki <inaki.diaz@ikasle.egibide.org>
2017-03-21 20:27:13 +0100

    creada escena game Over

---
+++ 02_unity/SpaceInvaders/Assets/Scripts/Game Over/gameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour {
+	public Button restartButton;
+	// Use this for initialization
+	void Start () {
+		Button btn = restartButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void TaskOnClick(){
+		SceneManager.LoadScene ("Nivel1");
+		Debug.Log ("You have clicked the button!");
+	}
+}
