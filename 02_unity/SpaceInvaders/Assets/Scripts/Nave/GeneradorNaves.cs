b3c833aac3f5fa7d85a89bc8e06263f75091bfaa
Iñaki <inaki.diaz@ikasle.egibide.org>
2017-03-28 22:00:25 +0200

    Pantalla start, generador naves one player, falta two players

---
+++ 02_unity/SpaceInvaders/Assets/Scripts/Nave/GeneradorNaves.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorNaves : MonoBehaviour {
+	public Rigidbody2D prefabNaveOne;
+	public Rigidbody2D naveTwo;
+
+	// Use this for initialization
+	void Start () {
+		
+		int players = PlayerPrefs.GetInt ("Player Number");
+
+		switch(players){
+		case 1:
+			// Posición de cada alien
+			Vector2 posicion = new Vector2 (0, -4);
+			// Instanciamos el objeto partiendo del prefab
+			Rigidbody2D naveOne = (Rigidbody2D)Instantiate (prefabNaveOne, posicion, transform.rotation);
+			break;
+		case 2:
+			
+			break;
+		default:
+			
+			break;			
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
