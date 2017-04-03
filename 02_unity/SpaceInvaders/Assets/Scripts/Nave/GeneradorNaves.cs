ca865078b6e72e721de606b9f5a736382ea6c3a9
inakidml <inaki.diaz@ikasle.egibide.org>
2017-04-03 11:19:46 +0200

    Pantalla inicio y dos players
    con A D y LeftAlt para disparo de la segunda nave

--- 02_unity/SpaceInvaders/Assets/Scripts/Nave/GeneradorNaves.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/Nave/GeneradorNaves.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class GeneradorNaves : MonoBehaviour {
 	public Rigidbody2D prefabNaveOne;
-	public Rigidbody2D naveTwo;
+	public Rigidbody2D prefabNaveTwo;
 
 	// Use this for initialization
 	void Start () {
 		
 		int players = PlayerPrefs.GetInt ("Player Number");
-
+		Vector2 posicion;
+		Rigidbody2D naveOne;
+		Rigidbody2D naveTwo;
 		switch(players){
 		case 1:
-			// Posición de cada alien
-			Vector2 posicion = new Vector2 (0, -4);
+			// Posición de la nave
+			posicion = new Vector2 (0, -4);
 			// Instanciamos el objeto partiendo del prefab
-			Rigidbody2D naveOne = (Rigidbody2D)Instantiate (prefabNaveOne, posicion, transform.rotation);
+			naveOne = (Rigidbody2D)Instantiate (prefabNaveOne, posicion, transform.rotation);
 			break;
 		case 2:
-			
+			// Posición de la nave 1
+			posicion = new Vector2 (-3, -4);
+			// Instanciamos el objeto partiendo del prefab
+			naveOne = (Rigidbody2D)Instantiate (prefabNaveOne, posicion, transform.rotation);
+
+			// Posición de la nave 2
+			posicion = new Vector2 (3, -4);
+			// Instanciamos el objeto partiendo del prefab
+			naveTwo = (Rigidbody2D)Instantiate (prefabNaveTwo, posicion, transform.rotation);
 			break;
 		default:
 			
