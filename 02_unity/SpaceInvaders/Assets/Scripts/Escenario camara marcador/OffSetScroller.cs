1946a83a4b84e4375cd0596e72b003238fa9a03c
inakidml <inaki.diaz@ikasle.egibide.org>
2017-04-08 23:31:02 +0200

    BakcGrounds con Quad Scroll, Naves Kinematic

---
+++ 02_unity/SpaceInvaders/Assets/Scripts/Escenario camara marcador/OffSetScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffSetScroller : MonoBehaviour {
+	//https://unity3d.com/es/learn/tutorials/topics/2d-game-creation/2d-scrolling-backgrounds
+	// Use this for initialization
+
+	public float scrollSpeed; 
+	private Vector2 savedOffset;
+	private Renderer rend;
+
+
+	void Start () {
+		//savedOffset = renderer.material.GetTextureOffset ("_MainTex");
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float y = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (0, y);
+		rend.material.SetTextureOffset ("_MainTex", offset);
+	}
+}
