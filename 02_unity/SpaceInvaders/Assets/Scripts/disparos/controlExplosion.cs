c3aaed768098056d2ec240efec24bc94be8ec57b
inakidml <inaki.diaz@ikasle.egibide.org>
2017-04-09 09:54:32 +0200

    timer para explosión con time.time

--- 02_unity/SpaceInvaders/Assets/Scripts/disparos/controlExplosion.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/disparos/controlExplosion.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class controlExplosion : MonoBehaviour {
 	private int contadorExplosion = 50;
+	private float timer;
 	// Use this for initialization
 	void Start () {
-		
+		timer = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (contadorExplosion <= 0) {
-			
+		float ahora = Time.time;
+		float difTiempo = ahora - timer;
+		if (difTiempo > 1.3) {			
 			Destroy (gameObject);
-		} else {
-			contadorExplosion--;
 		}
 	}
 }
